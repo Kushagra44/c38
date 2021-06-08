c25dbe2e1220ea6bc1fa447121ffd953e82a7c96
Kushagra44 <80953248+Kushagra44@users.noreply.github.com>
2021-06-08 12:47:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var gameState = 0
+var form, game,player
+var playerCount
+var database
+var allPlayers
+var distance=0
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database()
+game=new Game()
+game.getState()
+game.start()
+}
+
+function draw(){
+if(playerCount===4){
+    game.update(1)
+    
+}
+if(gameState===1){
+    clear()
+    game.play()
+}
+}
+
